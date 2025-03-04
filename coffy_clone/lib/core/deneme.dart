0334aac63b158d9b0e32d74f0a3e225653af9d78
FTccr <furkantuccar@gmail.com>
2025-03-04 15:41:32 +0300

    Deneme

---
+++ coffy_clone/lib/core/deneme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DenemeWidget extends StatelessWidget {
+  const DenemeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
