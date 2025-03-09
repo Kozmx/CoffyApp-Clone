4fb5371c6e0acf015a79c99f625f1a919040b568
FTccr <furkantuccar@gmail.com>
2025-03-09 13:43:40 +0300

    Added constants to be used for the project

---
+++ coffy_clone/lib/constants/Project-Items.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ColorItems {
+  static final Color brightGrey = Color(0xFFF4F5F7);
+  static final Color blackcolor = Colors.black;
+  static final Color whitecolor = Colors.white;
+  static final Color orangecolor = Color(0xFFF7941D);
+
+}
+
+class PaddingItems {
+  static const EdgeInsets symmetricPadding = EdgeInsets.symmetric(horizontal: 15, vertical: 8);
+  static const EdgeInsets symmetricText = EdgeInsets.symmetric(horizontal: 15,vertical: 0);
+  static const EdgeInsets topPadding = EdgeInsets.only(top: 15);
+  static const EdgeInsets bottomPadding = EdgeInsets.only(bottom:15 );
+   static const EdgeInsets symmetricListtile = EdgeInsets.symmetric(horizontal: 15,vertical: 5);
+  
+}
