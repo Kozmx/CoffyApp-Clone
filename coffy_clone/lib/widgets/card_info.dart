96cf5a1dfdca13d5ca042c0068b546384950ad2f
FTccr <furkantuccar@gmail.com>
2025-03-11 13:12:47 +0300

    Attırı bıttırı

---
+++ coffy_clone/lib/widgets/card_info.dart
@@ -0,0 +1,31 @@
+import 'package:coffy_clone/constants/Project-Items.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class CardInfo extends StatelessWidget {
+  final List<Widget> children;
+  final Color? backgroundColor;
+  const CardInfo({
+    super.key, required this.children,this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: PaddingItems.symmetricPadding,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: backgroundColor ?? ColorItems.whitecolor,
+        ),
+        width: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start, 
+          children: children, 
+            ),
+        ),
+      
+      );
+  }
+}
